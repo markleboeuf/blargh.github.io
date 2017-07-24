b551a5d7e4083b58d88c7434cf7484ab3e6621ad
markleboeuf <markleboeuf1@gmail.com>
2017-07-23 19:09:30 -0700

    add article on exception handling in R

---
+++ portfolio/error_handling.R
@@ -0,0 +1,5 @@
+strings = c("How now brown cow", "The arsonist had oddly shaped feet", 
+            "The skeleton ran out of shampoo in the shower", 
+            "The human torch was denied a bank loan",
+            "Snoopy was stabbed by a spear")
+(126000 * 1.03) + 129780 * 0.10 + 7000
